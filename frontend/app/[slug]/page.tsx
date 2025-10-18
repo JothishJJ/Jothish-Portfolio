14849726cb9b306ffd9a01d641579fde04e55c16
JothishJJ <jothishjj11@gmail.com>
2025-10-18 23:31:07 +0530

    Make the blogs visually more appealing

--- frontend/app/[slug]/page.tsx
+++ frontend/app/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { PortableText, type SanityDocument } from "next-sanity";
 import imageUrlBuilder from "@sanity/image-url";
 import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 import { client } from "@/sanity/client";
+
 import Link from "next/link";
 
 const POST_QUERY = `*[_type == "post" && slug.current == $slug][0]`;
@@ -14,19 +15,35 @@ const urlFor = (source: SanityImageSource) =>
 
 const options = { next: { revalidate: 30 } };
 
+export async function generateMetadata({params}: {params: Promise<{slug: string}>}) {
+  const post = await client.fetch<SanityDocument>(
+    POST_QUERY,
+    await params,
+    options,
+  );
+
+  return {
+    title: post.title,
+  }
+}
+
 export default async function PostPage({
   params,
 }: {
   params: Promise<{ slug: string }>;
 }) {
-  const post = await client.fetch<SanityDocument>(POST_QUERY, await params, options);
+  const post = await client.fetch<SanityDocument>(
+    POST_QUERY,
+    await params,
+    options,
+  );
   const postImageUrl = post.image
     ? urlFor(post.image)?.width(550).height(310).url()
     : null;
 
   return (
     <main className="container mx-auto min-h-screen max-w-3xl p-8 flex flex-col gap-4">
-      <Link href="/" className="hover:underline">
+      <Link href="/" className="hover:underline text-sm">
         ← Back to posts
       </Link>
       {postImageUrl && (
@@ -38,9 +55,10 @@ export default async function PostPage({
           height="310"
         />
       )}
-      <h1 className="text-4xl font-bold mb-8">{post.title}</h1>
+      <h1 className="text-4xl font-bold">{post.title}</h1>
+      <p className="text-sm">{new Date(post.publishedAt).toLocaleDateString()}</p>
+      <hr />
       <div className="prose">
-        <p>Published: {new Date(post.publishedAt).toLocaleDateString()}</p>
         {Array.isArray(post.body) && <PortableText value={post.body} />}
       </div>
     </main>
